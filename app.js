e2580df609e6051952d90af4bb2d13ca3d8f177c
Giftos1 <giftsydney676@gmail.com>
2023-03-19 15:45:07 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+
+function calculateBmi() {
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+    const results = document.querySelector('.results');
+    let bmiValue = weight / (height * height / 10000);
+    bmiValue = bmiValue.toFixed(2);
+
+    if (bmiValue > 0 && bmiValue < 18.5) {
+        results.textContent = "Your BMI is " + bmiValue + " hence you are Underweight!";
+    }
+    else if (bmiValue > 18.5 && bmiValue < 25) {
+        results.textContent = "Your BMI is " + bmiValue + " hence you have a Healthy weight:)";
+    }
+    else if (bmiValue > 25 && bmiValue < 30) {
+        results.textContent = "Your BMI is " + bmiValue + " hence you are overweight but not obese!";
+    }
+    else if (bmiValue >= 30 && bmiValue < 35 ){
+        results.textContent = "Your BMI is " + bmiValue + " hence you are Obese class I!";
+    }
+    else if (bmiValue >= 35 && bmiValue < 40 ){
+        results.textContent = "Your BMI is " + bmiValue + " hence you are Obese class II!";
+    }
+    else if (isNaN(weight) || isNaN(height)) {
+        alert("Please enter number values only!")
+        window.location.reload(true); // reload page fully from server and not local cache. 
+        // Leave blank or use false if you want to reload from local cache.
+    }
+    else {
+        alert("Please key in positive values!")
+        window.location.reload(true);
+    }
+}
+
+
+
